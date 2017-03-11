da2b6aca7719fc69ca1936fb09d35b6e7386208c
zidanepc <zidanepc@DESKTOP-VIIUN1E>
2017-03-11 12:17:33 +0800

    添加项目文件。

---
+++ WhatsCool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhatsCool.Startup))]
+namespace WhatsCool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
